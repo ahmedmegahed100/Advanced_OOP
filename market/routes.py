5dee73fdd67c4ed6cdc400b5751e6f15fc67d290
ahmedmegahed100 <afsahmedmucahit@gmail.com>
2023-12-23 15:19:41 +0300

    register messages fix

--- market/routes.py
+++ market/routes.py
@@ -61,6 +61,12 @@ def register_page():
         else:
             flash('Username or email address already exists! Please try a different one', category='danger')
 
+    if form.errors != {}:  # If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f'There was an error with creating a user: {err_msg}', category='danger')
+
+    return render_template('register.html', form=form)
+
     return render_template('register.html', form=form)
 
 
